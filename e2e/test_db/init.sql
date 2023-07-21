5340282b1feb1100996e56f07c9e6456c425449a
João Gonçalves <joao.goncalves01@gmail.com>
2023-07-21 14:13:15 +0000

    remove trigger to fix error in ci

--- e2e/test_db/init.sql
+++ e2e/test_db/init.sql
@@ -24,18 +24,18 @@ CREATE TABLE IF NOT EXISTS Vessel_location.swarm_events(
 );
 
 
--- Create a trigger to automatically calculate signal_quality
-CREATE TRIGGER calculate_signal_quality BEFORE INSERT ON Vessel_location.swarm_events
-FOR EACH ROW
-BEGIN
-  SET NEW.signal_quality =
-    CASE
-      WHEN NEW.rssi_dbm < -120 OR NEW.snr_db < -20 THEN 'Very Poor'
-      WHEN (NEW.rssi_dbm >= -120 AND NEW.rssi_dbm <= -110) OR (NEW.snr_db >= -20 AND NEW.snr_db <= -10) THEN 'Poor'
-      WHEN (NEW.rssi_dbm >= -110 AND NEW.rssi_dbm <= -100) OR (NEW.snr_db >= -10 AND NEW.snr_db <= 0) THEN 'Fair'
-      WHEN (NEW.rssi_dbm >= -100 AND NEW.rssi_dbm <= -90) OR (NEW.snr_db >= 0 AND NEW.snr_db <= 10) THEN 'Good'
-      WHEN NEW.rssi_dbm > -90 OR NEW.snr_db > 10 THEN 'Excellent'
-      ELSE 'Unknown'
-    END;
-END;
+-- -- Create a trigger to automatically calculate signal_quality
+-- CREATE TRIGGER calculate_signal_quality BEFORE INSERT ON Vessel_location.swarm_events
+-- FOR EACH ROW
+-- BEGIN
+--   SET NEW.signal_quality =
+--     CASE
+--       WHEN NEW.rssi_dbm < -120 OR NEW.snr_db < -20 THEN 'Very Poor'
+--       WHEN (NEW.rssi_dbm >= -120 AND NEW.rssi_dbm <= -110) OR (NEW.snr_db >= -20 AND NEW.snr_db <= -10) THEN 'Poor'
+--       WHEN (NEW.rssi_dbm >= -110 AND NEW.rssi_dbm <= -100) OR (NEW.snr_db >= -10 AND NEW.snr_db <= 0) THEN 'Fair'
+--       WHEN (NEW.rssi_dbm >= -100 AND NEW.rssi_dbm <= -90) OR (NEW.snr_db >= 0 AND NEW.snr_db <= 10) THEN 'Good'
+--       WHEN NEW.rssi_dbm > -90 OR NEW.snr_db > 10 THEN 'Excellent'
+--       ELSE 'Unknown'
+--     END;
+-- END;
 
